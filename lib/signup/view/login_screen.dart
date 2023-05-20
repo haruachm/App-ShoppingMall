095b3bfd7e40d15b50a11b81963f1f13943af28c
haruachm <85959639+haruachm@users.noreply.github.com>
2023-05-21 04:04:35 +0900

    feat: 로그인 API 연동 + Platform별 IP 라우팅

--- lib/signup/view/login_screen.dart
+++ lib/signup/view/login_screen.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
@@ -6,12 +9,25 @@ import 'package:shopping_mall/common/const/colors.dart';
 import 'package:shopping_mall/common/layout/default_layout.dart';
 import 'package:dio/dio.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  String username = '';
+  String password = '';
 
   @override
   Widget build(BuildContext context) {
     final dio = Dio();
+
+    final emulatorIP = '10.0.2.2:3000'; //안드로이드 IP
+    final simulatorIP = '127.0.0.1:3000'; //iOS IP
+
+    final ip = Platform.isIOS ? simulatorIP : emulatorIP;
 
     return DefaultLayout(
       child: SafeArea(
@@ -23,14 +39,35 @@ class LoginScreen extends StatelessWidget {
             children: [
               CustomTextFormField(
                 hintText: '이메일을 입력해주세요',
+                onChagned: (String value) {
+                  username = value;
+                },
               ),
               const SizedBox(height: 16.0),
               CustomTextFormField(
                 hintText: '비밀번호를 입력해주세요',
+                onChagned: (String value) {
+                  password = value;
+                },
                 obscureText: true,
               ),
               ElevatedButton(
-                onPressed: () async {},
+                onPressed: () async {
+                  final rawString = '$username:$password';
+                  print(rawString);
+                  Codec<String, String> stringToBase64 = utf8.fuse(base64);
+
+                  String token = stringToBase64.encode(rawString);
+
+                  final response = await dio.post(
+                    'http://$ip/auth/login',
+                    options: Options(
+                      headers: {
+                        'authorization': 'Basic $token',
+                      },
+                    ),
+                  );
+                },
                 style: ElevatedButton.styleFrom(
                   backgroundColor: primaryColor,
                 ),
